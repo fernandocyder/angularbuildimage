65825b44e0b1cd9c7447bad1127ce905be6d9ef2
Fernando Karnagi <fernando@cyder.com.sg>
2021-07-24 11:00:42 +0800

    Updated docker

---
+++ build.sh
@@ -0,0 +1,8 @@
+export IMAGE_NAME=fe_staff_portal
+export IMAGE_TAG=742070012776.dkr.ecr.ap-southeast-1.amazonaws.com/fe_staff_portal
+export IMAGE_VERSION=latest
+
+docker rmi -f $IMAGE_NAME
+docker build -f Dockerfile-fe-frontend-app -t $IMAGE_NAME:$IMAGE_VERSION .
+docker tag $IMAGE_NAME:$IMAGE_VERSION $IMAGE_TAG:$IMAGE_VERSION
+# docker push $IMAGE_NAME
