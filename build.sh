8b679b2fd0f48418e3b939d397f8c3e1848b0f63
Fernando Karnagi <fernando@cyder.com.sg>
2021-07-24 11:18:51 +0800

    UPDATE

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@ export IMAGE_NAME=fe_staff_portal
 export IMAGE_TAG=742070012776.dkr.ecr.ap-southeast-1.amazonaws.com/fe_staff_portal
 export IMAGE_VERSION=latest
 
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 742070012776.dkr.ecr.ap-southeast-1.amazonaws.com
+
 # docker rmi -f $IMAGE_NAME
 docker build -f Dockerfile-fe-frontend-app -t $IMAGE_NAME:$IMAGE_VERSION .
 docker tag $IMAGE_NAME:$IMAGE_VERSION $IMAGE_TAG:$IMAGE_VERSION
-# docker push $IMAGE_NAME
+
+docker push $IMAGE_TAG:$IMAGE_VERSION
